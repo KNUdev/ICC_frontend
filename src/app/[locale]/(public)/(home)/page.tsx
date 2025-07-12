2d15c785fff6dae00faba5bb0af0b0374f50df63
EvheniySlobodyanik <exiletopchik@gmail.com>
2025-07-12 15:54:51 +0300

    FormLines v1

--- src/app/[locale]/(public)/(home)/page.tsx
+++ src/app/[locale]/(public)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import { Form } from '@/components/Form/Form'
+import { FormLines } from '@/components/Form/FormApplication/FormLines/FormLines'
 
 export default function HomePage() {
   return (
     <div>
       <Form />
+      <FormLines />
     </div>
   )
 }
---
+++ src/components/Form/FormApplication/FormLines/FormLines.tsx
@@ -0,0 +1,22 @@
+import { FormApplication } from '../FormApplication'
+import LinesLeft from '@/assets/image/icons/lines-left.svg'
+import LinesRight from '@/assets/image/icons/lines-right.svg'
+import styles from './FormLines.module.scss'
+import { useTranslations } from 'next-intl'
+
+export function FormLines() {
+  const tFormText = useTranslations('form/text')
+
+  return (
+    <section className={styles.sectionLines}>
+      <LinesLeft />
+
+      <div className={styles.formContainer}>
+        <h1 className={styles.formHeading}>{tFormText('linesHeading')}</h1>
+        <FormApplication />
+      </div>
+
+      <LinesRight />
+    </section>
+  )
+}
