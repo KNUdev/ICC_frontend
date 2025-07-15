ba2b35eabe9ac8422be5acc749e7be85f93c5371
Yakovleva Elizaveta <Lizayakovleva2005@gmail.com>
2025-07-15 15:15:53 +0300

    changed px to rem and svg in individual files

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,40 +1,24 @@
-'use client';
-
 import { Footer } from '@/components/Footer/Footer';
-import Accordion from '@/components/Accordion/Accordion';
+import FAQ from '@/components/FAQ/FAQ';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
-import styles from '@/components/Accordion/Accordion.module.scss';
-
 import { useTranslations } from 'next-intl';
 
 export default function HomePage() {
   const t = useTranslations('home');
 
-  const faqItems = t.raw('faq.items') as { title: string; content: string }[];
-
-  const accordionItems = faqItems.map((item, index) => ({
-    id: `faq-${index}`,
-    title: item.title,
-    content: <p>{item.content}</p>,
-  }));
-
   return (
-    <div>
+    <main>
       <LanguageSwitcher />
-      <h1 >{t('title')}</h1>
+      <h1>{t('title')}</h1>
 
       <a href="#faq" style={{ textDecoration: 'underline', color: 'blue' }}>
-        Часті запитання
+        {t('faq.title')}
       </a>
 
-      <div style={{ height: '400px' }} />
+      <div style={{ height: '40rem' }} /> 
 
-      <section id="faq">
-        <h2 className={styles.sectionTitle}>{t('faq.title')}</h2>
-        <Accordion items={accordionItems} />
-      </section>
-
+      <FAQ />
       <Footer />
-    </div>
+    </main>
   );
 }
