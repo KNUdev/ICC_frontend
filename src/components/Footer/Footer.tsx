3d327a88b6b689bc68a5a81237f3e7c3df848b50
EvheniySlobodyanik <exiletopchik@gmail.com>
2025-07-27 16:09:01 +0300

    design watermark footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -161,6 +161,8 @@ export function Footer() {
             </ContactLink>
           </div>
         </address>
+
+        <p className={styles.designWatermark}>Designed by Bohdan Popov & ICC</p>
       </div>
     </footer>
   )
