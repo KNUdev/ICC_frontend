78424b57b26bc3552ee98a1af3623282fdd6dc1a
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-10-03 18:16:08 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: 'iccknu.pp.ua',
+          hostname: 'multiparous-waggly-krysta.ngrok-free.dev',
           pathname: '/images/**',
         },
       ],
