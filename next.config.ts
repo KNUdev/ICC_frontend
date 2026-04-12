f9eb9d519f500a0bb09eca799e04fbe94a23755d
Serhii Zelinskyi <serhii.zelinskyi@knu.ua>
2026-04-12 23:41:25 +0300

    app: add docker setup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 import withPWAInit from 'next-pwa'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
